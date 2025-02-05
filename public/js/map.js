38da67efa1d660cb52cb898093d5147b430a4c80
ChristianWeersink <christian.weersink@dcmail.ca>
2025-02-04 22:20:34 -0500

    refactored map

--- public/js/map.js
+++ public/js/map.js
@@ -9,8 +9,8 @@ let openNow = false;
 const maxResults = 10; //1-20 results
 const mapZoom = 12; //zoom value for the map
 const mapContainer = document.getElementById('map');
-let markers = []; // Store all map markers
-let searchCenter = {lat: 1 , lng: 1};
+let markers = []; // Store all map markers for removal 
+let searchCenter = {lat: 1 , lng: 1}; // default search center, this gets changed when the user searches for a location
 
 
 // This function will be called once the Google Maps API is loaded
@@ -23,67 +23,6 @@ function initMap() {
     }
     
 }
-
-// Search for nearby vet offices
-async function nearbySearch(center) {
-    try{
-        const service = new google.maps.places.PlacesService(vetMap);
-        markers.forEach(marker => marker.setMap(null)); // Remove markers from the map
-        markers = [];
-    
-        // Create the text search query string
-        const query = `${searchTerm} veterinary office`;
-        const bounds = new google.maps.LatLngBounds();
-        const northEast = google.maps.geometry.spherical.computeOffset(center, radius, 45); // 45 degrees for NE corner
-        const southWest = google.maps.geometry.spherical.computeOffset(center, radius, 225); // 225 degrees for SW corner
-
-        bounds.extend(northEast);
-        bounds.extend(southWest);
-        var fields = ['location', 'id',];
-        // check if the user wants only open locations
-        const request = {
-            textQuery: query,  // The query string is used here for text search
-            fields: fields, // You can include additional fields based on your need
-            maxResultCount: maxResults,
-            includedType: "veterinary_care",
-            locationRestriction: bounds,
-            isOpenNow: openNow,
-        };
-        const { Place } = await google.maps.importLibrary("places");
-    
-        const { places } = await Place.searchByText(request);
-    
-            if (places.length > 0) {
-                const bounds = new google.maps.LatLngBounds();
-                places.forEach((place) => {
-                    // Use AdvancedMarkerElement for nearby places
-                    const marker = new google.maps.marker.AdvancedMarkerElement({
-                        map: vetMap,
-                        position: place.location,
-                        id: place.id,
-                        gmpClickable: true,
-                    });
-                    markers.push(marker); // store the marker to delete later
-                    marker.addListener("click", ({ domEvent, latLng }) => {
-                        const { target } = domEvent;
-                        const placeDetail = fetchPlaceDetails(place);
-                      });
-                    
-                    bounds.extend(place.location);
-                });
-                // Extend the map screen to fit user and add user marker to the map
-                bounds.extend(center);
-                addUserMarker(center);
-                vetMap.fitBounds(bounds);
-            } else {
-                setMap(mapZoom, center);
-                alert("No vets found in this area");
-            }
-    }
-    catch(e){
-        console.log(e)
-    }
-};
 
 // Add "You Are Here" marker using AdvancedMarkerElement
 function addUserMarker(userLatLng) {
@@ -113,7 +52,7 @@ function createCustomMarkerContent() {
     markerDiv.style.alignItems = 'center';
     markerDiv.style.color = 'white';
     markerDiv.style.fontSize = '12px';
-    markerDiv.innerText = ''; // Initial for "User"
+    markerDiv.innerText = ''; //
     return markerDiv;
 }
 // Set map variable to specified zoom and center position zoom, int and center latlng
@@ -228,51 +167,6 @@ function loadMap(){
         setCookie("user", "true", 1);
 }
 
-// Calculate distance
-function calculateDistance(userLat, userLng, targetLat, targetLng) {
-    const R = 6371; // Radius of the Earth in km
-    const dLat = toRadians(targetLat - userLat);
-    const dLng = toRadians(targetLng - userLng);
-    const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-              Math.cos(toRadians(userLat)) * Math.cos(toRadians(targetLat)) *
-              Math.sin(dLng / 2) * Math.sin(dLng / 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    var num = R * c;
-    num = Math.round(num * 100) / 100;
-    return num; // Distance in km
-}
-// Helper function to convert degrees to radians
-function toRadians(degrees) {
-    return degrees * (Math.PI / 180);
-}
-
-async function searchByLocation() {
-    const locationInput = document.getElementById("location").value;
-    try {
-        const geocoder = new google.maps.Geocoder();
-        
-        geocoder.geocode({ address: locationInput }, async (results, status) => {
-            if (status === "OK") {
-                const newCenter = results[0].geometry.location;
-                searchCenter.lat = newCenter.lat(); // set search center to the new center so distance can be calc'd
-                searchCenter.lng = newCenter.lng();
-                console.log(newCenter);
-                // Move the map to the new location
-                vetMap.setCenter(newCenter);
-                vetMap.setZoom(13); // Adjust zoom level for city-wide searches
-
-                // Perform a search at the new location
-                await nearbySearch(newCenter);
-                addUserMarker(newCenter);
-            } else {
-                alert("Geocode was not successful for the following reason: " + status);
-            }
-        });
-    } catch (error) {
-        console.log("Error in geocoding:", error);
-    }
-}
-
 
 document.getElementById("locationBox").addEventListener("change", () => {
     if (document.getElementById("locationBox").checked) {
@@ -283,41 +177,3 @@ document.getElementById("locationBox").addEventListener("change", () => {
 });
 
 
-function executeSearch() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-            function (position) {
-                // Get user location 
-                userLatLng = {
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude,
-                };
-
-                // retrieve search term, radius, and openNow status
-                searchTerm = document.getElementById("type").value;
-                radius = document.getElementById("radius").value;
-                openNow = document.getElementById("openNow").checked;
-
-                // If the search term is "any", we want to make it empty
-                if (searchTerm === "any") {
-                    searchTerm = "";
-                }
-
-                // search by location if the box is checked to do a text search
-                if(document.getElementById("locationBox").checked){
-                    searchByLocation();
-                }
-                else{
-                    searchCenter = userLatLng;
-                    nearbySearch(searchCenter);
-                }
-                
-            },
-            function (error) {
-                alert("Error getting user location: " + error.message);
-            }
-        );
-    } else {
-        alert("Geolocation is not supported by this browser.");
-    }
-}
