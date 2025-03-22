e8908708c0eccedb9b57cb10ae8a596e9572ce13
ChristianWeersink <christian.weersink@dcmail.ca>
2025-03-21 23:03:46 -0400

    Update pet_profile.js

--- routes/pet_profile.js
+++ routes/pet_profile.js
@@ -31,14 +31,12 @@ router.get('/', async (req, res) => {
         // Render the EJS template with the correct pets data
         
         // Qr code options 
-        const options = {
-            width: 75, // desired width in pixels
-        };
+    
         for (let pet of pets) {
             petinfo = await fetchVetName(pet.pet_homevet_id);
             pet.vet_name = petinfo.name;
             pet.phone = petinfo.phone;
-            const qrCodeUrl = await QRCode.toDataURL(`https://toebeans.dev/qr_code/${pet.id}`, options);
+            const qrCodeUrl = await QRCode.toDataURL(`https://toebeans.dev/qr_code/${pet.id}`);
             pet.qrCode = qrCodeUrl;
         }
 
