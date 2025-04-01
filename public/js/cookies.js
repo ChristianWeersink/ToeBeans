23fd53cdec6e19294be839074018208a40606016
ChristianWeersink <christian.weersink@dcmail.ca>
2025-04-01 16:31:29 -0400

    theme update
    theme looking good and working better now

--- public/js/cookies.js
+++ public/js/cookies.js
@@ -29,3 +29,12 @@ function deleteCookie(name) {
     document.cookie = name + "=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC";
 }
 window.deleteCookie = deleteCookie;
+
+
+function showToast(message, title = "Notification") {
+  const toastEl = $('#liveToast');
+  $('#toastBody').text(message);
+  $('#toastTitle').text(title);
+  toastEl.toast('show');
+}
+window.showToast = showToast;
